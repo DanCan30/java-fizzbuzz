90eb763113f16f83c18cf880910dfc8a782fcba5
DanCan30 <danielecan3008@gmail.com>
2022-11-15 17:29:14 +0100

    Init commit

---
+++ src/org/generation/italy/FizzBuzz.java
@@ -0,0 +1,22 @@
+package org.generation.italy;
+
+public class FizzBuzz {
+	public static void main(String[] args) {
+		int[] numbers = new int[100];
+		
+		for(int i = 0; i < numbers.length; i++) {
+			numbers[i] = i + 1;
+			
+			if (numbers[i] % 3 == 0 && numbers[i] % 5 == 0) {
+				System.out.println("FizzBuzz");
+			} else if (numbers[i] % 3 == 0) {
+				System.out.println("Fizz");
+			} else if (numbers[i] % 5 == 0) {
+				System.out.println("Buzz");
+			} else {
+								
+				System.out.println(numbers[i]);
+			}
+		}
+	}
+}
